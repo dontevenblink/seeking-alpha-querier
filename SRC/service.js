6fb0726b34cd33595cb4b43a39a24ee640b71c6d
Samuel Rees <samuel.rees@gmail.com>
2022-03-11 19:11:42 -0500

    targets es2021

--- SRC/service.js
+++ SRC/service.js
@@ -40,11 +40,10 @@ class Options {
 let collector;
 const doc = new GoogleSpreadsheet(`${process.env.SHEET_ID}`);
 (async function () {
-    var _a;
     try {
         await doc.useServiceAccountAuth({
             client_email: process.env.CLIENT_EMAIL,
-            private_key: (_a = process.env.PRIVATE_KEY) === null || _a === void 0 ? void 0 : _a.replace(/\\n/g, '\n'),
+            private_key: process.env.PRIVATE_KEY?.replace(/\\n/g, '\n'),
         });
         await doc.loadInfo();
         main();
