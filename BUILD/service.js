e6b399089c46fd4e127d11351662110969c9e426
Samuel Rees <samuel.rees@gmail.com>
2022-03-19 23:41:00 -0400

    last commit

--- BUILD/service.js
+++ BUILD/service.js
@@ -1,4 +1,5 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv = require('dotenv');
 if (process.env.RUN_ENV !== 'production') {
     dotenv.config();
@@ -37,59 +38,56 @@ class Options {
             });
     }
 }
-let collector;
+let collector = [];
 const doc = new GoogleSpreadsheet(`${process.env.SHEET_ID}`);
-start();
-async function start() {
+let sheet;
+main();
+// functions
+async function main() {
     try {
         await doc.useServiceAccountAuth({
             client_email: process.env.CLIENT_EMAIL,
             private_key: process.env.PRIVATE_KEY?.replace(/\\n/g, '\n'),
         });
         await doc.loadInfo();
-        main();
+        sheet = doc.sheetsByIndex[0];
+        await asyncForEach(tickers, loopableRequest);
     }
     catch (error) {
         console.log(`error: ${error}`);
     }
 }
-function main() {
-    const sheet = doc.sheetsByIndex[0];
-    console.log(`sheet title: ${sheet.title}`);
-    asyncForEach(tickers, async function (e) {
-        if (collector.length < 4)
-            collector.push(e);
-        if (collector.length === 4 || e === tickers[tickers.length - 1]) {
-            console.log(`about to request these tickers: ${collector}`);
-            let options = new Options(collector);
-            await request(options, sheet);
-            console.log('resetting collector...');
-            collector = [];
-        }
-    });
+async function loopableRequest(e) {
+    if (collector.length < 4)
+        collector.push(e);
+    if (collector.length === 4 || e === tickers[tickers.length - 1]) {
+        let options = new Options(collector);
+        await requestAndPost(options);
+        console.log('resetting collector...');
+        collector = [];
+    }
 }
 async function asyncForEach(array, callback) {
     for (let index = 0; index < array.length; index++) {
         await callback(array[index], index, array);
     }
 }
-async function request(options, sheet) {
-    await axios
-        .request(options)
-        .then(async function (response) {
-        // console.log(response.data.data);
-        await asyncForEach(response.data.data, async (element) => {
-            // console.log(element.attributes.pegNongaapFy1);
-            const newRow = await sheet.addRow({
-                ticker: element.id,
-                pegFWD: element.attributes.pegNongaapFy1,
-                pegTTM: element.attributes.pegRatio,
-                date: spacetime.now('America/New_York').format('{iso-month}/{date-pad}/{year}'),
-            });
-            console.log('postToSheetComplete');
-        });
-    })
-        .catch((error) => {
-        console.error(error);
+async function requestAndPost(options) {
+    console.log(`requesting data on these tickers: ${collector}...`);
+    let response = await axios.request(options);
+    await loopPost(response);
+}
+async function loopPost(response) {
+    // console.log(response.data.data);
+    await asyncForEach(response.data.data, postToSheet);
+}
+async function postToSheet(element) {
+    console.log('posting to sheet...');
+    const newRow = await sheet.addRow({
+        ticker: element.id,
+        pegFWD: element.attributes.pegNongaapFy1,
+        pegTTM: element.attributes.pegRatio,
+        date: spacetime.now('America/New_York').format('{iso-month}/{date-pad}/{year}'),
     });
+    // console.log('postToSheetComplete');
 }
